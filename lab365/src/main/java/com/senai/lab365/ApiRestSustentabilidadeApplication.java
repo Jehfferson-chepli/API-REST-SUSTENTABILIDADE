c7c03fc9beb7af66aa710088bb0aa7c2769b8166
Jehfferson-chepli <jehfferson_chepli@estudante.sesisenai.org.br>
2025-09-07 21:09:12 -0300

    [M03S01] API Sustentável - Ações pelo Futuro
    Desenvolver uma API REST que permita o cadastro, consulta, atualização e remoção de ações sustentáveis promovidas por pessoas físicas, ONGs ou empresas. As ações podem ser doações, mutirões, reciclagens, plantio de árvores, entre outras. A API deve seguir boas práticas com Spring Boot, Spring Data JPA, validações com Bean Validation, tratamento de exceções customizadas e controle de versão com Git.

---
+++ lab365/src/main/java/com/senai/lab365/ApiRestSustentabilidadeApplication.java
@@ -0,0 +1,13 @@
+package com.senai.lab365;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSustentabilidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSustentabilidadeApplication.class, args);
+	}
+
+}
---
+++ lab365/src/test/java/com/senai/lab365/ApiRestSustentabilidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.lab365;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSustentabilidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
